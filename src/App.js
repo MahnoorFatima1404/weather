7f0b19a6dfb8a10f23122648817561734da238ca
MahnoorFatima1404 <116874131+MahnoorFatima1404@users.noreply.github.com>
2022-10-28 13:19:34 +0500

    weather app

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [weather, updateWeather] = useState([]);
+  const [city, updateCity] = useState("");
+
+  const chckweather = () => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ed226b5573440fa7c1c35654bef023fa`
+    )
+      .then((data) => data.json())
+      .then((data) => {
+        updateWeather(data);
+      })
+      .catch(() => {});
+  };
+  return (
+    <div className="App">
+      <input
+        type={"text"}
+        placeholder="Enter City Name"
+        value={city}
+        onChange={(e) => updateCity(e.target.value)}
+      />
+      <button className="btn btn-primary m-5" onClick={chckweather}>
+        Search Weather
+      </button>
+      <h3>{weather?.name}</h3>
+      <h3>{weather?.main?.temp}</h3>
+    </div>
+  );
+}
+
+export default App;
